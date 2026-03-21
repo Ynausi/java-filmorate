26cca0124e3494c99806b0fc352f93a53a267e5e
Ynausi <trot55636@gmail.com>
2026-03-21 16:14:02 +0300

    mege with likes

--- src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Genre.java
@@ -7,12 +7,14 @@ public enum Genre {
     THRILLER("Триллер"),
     DOCUMENTARY("Документальный"),
     ACTION("Боевик");
+
     private String title;
+    public String getTitle() {
+        return title;
+    }
 
     Genre(String title) {
         this.title = title;
     }
-    public String getTitle() {
-        return title;
-    }
+
 }
