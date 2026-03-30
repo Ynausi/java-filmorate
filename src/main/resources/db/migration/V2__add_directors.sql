95683f08f8de55e56745d7d093acb8b711100e6c
Kirill-Kalinin-2025 <kirya.kalina.06@yandex.ru>
2026-03-30 18:30:11 +0300

    add remove endpoint v2

--- src/main/resources/db/migration/V2__add_directors.sql
+++ src/main/resources/db/migration/V2__add_directors.sql
@@ -1,10 +1,13 @@
+-- Создаём таблицу режиссёров
 CREATE TABLE IF NOT EXISTS Directors (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
-ALTER TABLE Films DROP COLUMN director;
+
+-- Добавляем столбец directorId в таблицу Films
 ALTER TABLE Films ADD COLUMN directorId INT;
 
+-- Добавляем внешний ключ
 ALTER TABLE Films
     ADD CONSTRAINT fk_films_director
     FOREIGN KEY (directorId) REFERENCES Directors(id);
