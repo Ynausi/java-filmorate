439865b837ed499c608399d0ccf9cb94565e0788
Ynausi <trot55636@gmail.com>
2026-03-31 06:36:19 +0300

    fix directors problem

--- src/main/java/ru/yandex/practicum/filmorate/Repository/DirectorRepositoryImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/Repository/DirectorRepositoryImpl.java
@@ -4,26 +4,35 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Director;
-import java.util.Collection;
+
 import java.util.Optional;
+import java.util.*;
 
 @Repository
 public class DirectorRepositoryImpl extends BaseRepository implements DirectorRepository{
     private static final String FIND_ALL_DIRECTORS = "SELECT * FROM Directors";
     private static final String FIND_BY_ID = "SELECT * FROM Directors WHERE id = ?";
-    private static final String ADD_DIRECTOR = "INSERT INTO Directors(name) " +
-                                                "VALUES(?)";
-    private static final String UPDATE_DIRECTOR = "UPDATE Directors SET " +
-                                                    "name = ? WHERE id = ?";
-    private static final String DELETE_DIRECTOR = "DELETE * FROM Directors WHERE id = ?";
+    private static final String ADD_DIRECTOR = "INSERT INTO Directors(name) VALUES(?)";
+    private static final String UPDATE_DIRECTOR = "UPDATE Directors SET name = ? WHERE id = ?";
+    private static final String DELETE_DIRECTOR = "DELETE FROM Directors WHERE id = ?";
+    private static final String FIND_DIRECTORS_FOR_FILM = "SELECT d.id, d.name " +
+            "FROM Directors AS d " +
+            "JOIN Film_Directors as fd ON d.id = fd.directorId " +
+            "WHERE fd.filmId = ? " +
+            "ORDER BY d.id";
 
     public DirectorRepositoryImpl(JdbcTemplate jdbc, RowMapper<Director> mapper) {
         super(jdbc, mapper);
     }
 
     @Override
-    public Collection<Director> findAll() {
-        return findMany(FIND_ALL_DIRECTORS);
+    public Set<Director> findAll() {
+        return new LinkedHashSet<>(findMany(FIND_ALL_DIRECTORS));
+    }
+
+    @Override
+    public Set<Director> findAllDirectorsForFilm(int filmId) {
+        return new LinkedHashSet<>(findMany(FIND_DIRECTORS_FOR_FILM, filmId));
     }
 
     @Override
