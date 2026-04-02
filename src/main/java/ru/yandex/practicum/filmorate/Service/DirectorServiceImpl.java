4f3c664e264c8670231d85335b4bf90d5a737b02
Ynausi <trot55636@gmail.com>
2026-04-02 22:27:53 +0300

    fix director validation and delete

--- src/main/java/ru/yandex/practicum/filmorate/Service/DirectorServiceImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/Service/DirectorServiceImpl.java
@@ -41,6 +41,10 @@ public class DirectorServiceImpl implements DirectorService {
         if (directorRepository.findById(directorId).isEmpty()) {
             throw new NotFoundException("No director with id: " + directorId);
         }
-        return directorRepository.delete(directorId);
+        boolean deleted = directorRepository.delete(directorId);
+        if (!deleted) {
+            throw new NotFoundException("No director with id: " + directorId);
+        }
+        return true;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/Director.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Director.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Director {
     private Integer id;
+    @NotBlank
     private String name;
 }
